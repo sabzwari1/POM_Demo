a62602f15ac2b229994390b7cde8f6676e00f7be
Syed Umair <umair.sabzwari@gmail.com>
2016-08-31 15:49:56 -0500

    new

--- PageObjectModel_WithPageFactory/src/test/java/com/umair/facebook/pom/testcases/ProfileTest.java
+++ PageObjectModel_WithPageFactory/src/test/java/com/umair/facebook/pom/testcases/ProfileTest.java
@@ -24,7 +24,7 @@ public class ProfileTest {
 		driver.manage().window().maximize();
 		LaunchPage launchpage = PageFactory.initElements(driver, LaunchPage.class);
 		LoginPage loginPage = launchpage.gotoLoginPage();
-		Object page = loginPage.doLogin("xhot_2handle@msn.com", "");
+		Object page = loginPage.doLogin("", "");
 		if (page instanceof LoginPage) {
 			Assert.fail("Login Failed");
 		} else if (page instanceof LandingPage) {
